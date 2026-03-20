9b041186276fa393a3ab7c0475f9cf5a0329f60a
Najeeb Siddiqui <najeeb@ssgqatar.com>
2026-03-20 09:58:01 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -86,7 +86,7 @@ export default function Home() {
       <Header />
 
       {/* Hero Banner */}
-      <div className="pt-20 flex items-center justify-center" style={{ backgroundColor: '#333333', minHeight: '220px' }} style={{ minHeight: '220px' }}>
+      <div className="pt-20 flex items-center justify-center" style={{ backgroundColor: '#333333', minHeight: '220px' }} >
         <SSGLogo className="w-full max-w-lg h-auto px-8" />
       </div>
 
